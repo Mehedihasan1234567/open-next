6000300644a89ccb3b55b3405e3a3b8ced96aeff
Mehedi hasan <mehedihasan66t@gmail.com>
2026-01-20 17:28:17 +0600

    This should resolve the issue with the WebAssembly modules

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["@vercel/og"],
 };
 
 export default nextConfig;
--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,14 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  wasm: [
+    {
+      name: "resvg_wasm",
+      path: require.resolve("next/dist/compiled/@vercel/og/resvg.wasm"),
+    },
+    {
+      name: "yoga_wasm",
+      path: require.resolve("next/dist/compiled/@vercel/og/yoga.wasm"),
+    },
+  ],
+});
