40b096a7c16c9d618aadd65ef433ea6906e36a85
Mehedi hasan <mehedihasan66t@gmail.com>
2026-01-20 17:40:09 +0600

    new build issue fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  wasm: [
-    {
-      name: "resvg_wasm",
-      path: require.resolve("next/dist/compiled/@vercel/og/resvg.wasm"),
-    },
-    {
-      name: "yoga_wasm",
-      path: require.resolve("next/dist/compiled/@vercel/og/yoga.wasm"),
-    },
-  ],
-});
+export default defineCloudflareConfig();
